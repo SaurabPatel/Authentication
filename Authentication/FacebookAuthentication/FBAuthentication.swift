6a9c01b666d7961c83d765c465696f64f7e8a819
SaurabhPatel <40667243+SaurabPatel@users.noreply.github.com>
2022-02-06 00:30:56 +0530

    Update FBAuthentication.swift

--- Authentication/FacebookAuthentication/FBAuthentication.swift
+++ Authentication/FacebookAuthentication/FBAuthentication.swift
@@ -5,62 +5,68 @@ import FBSDKLoginKit
 class FBSignInHelper {
     
     public static var shared = FBSignInHelper()
+   
     
-    func signIn() {
+    func signIn(user: @escaping (_ result: Any?) -> Void) {
         
         
         //if the user is already logged in
+        
         if let token = AccessToken.current,
-                !token.isExpired {
-            getFBUserData()
-            return
-            }
+           !token.isExpired {
+            getFBUserData(completion: { result in
+                user(result)
+            })
+        }
         //Get Currunt RootView
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
         guard let rootViewController = windowScene.windows.first?.rootViewController else { return }
         
         let loginManager = LoginManager()
         loginManager.logIn(permissions: [ .publicProfile ], viewController: rootViewController) { loginResult in
-                    switch loginResult {
-                    case .failed(let error):
-                        print(error)
-                    case .cancelled:
-                        print("User cancelled login.")
-                    case .success(let grantedPermissions, let declinedPermissions, let accessToken):
-                        self.getFBUserData()
-                    }
-                }
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                self.getFBUserData(completion: { result in
+                    user(result)
+                })
+            }
+        }
     }
     
-    func getFBUserData(){
+    private func getFBUserData(completion: @escaping (_ result: Any?) -> Void){
         if((AccessToken.current) != nil){
             GraphRequest(graphPath: "me",
-                                parameters: ["fields": "id, name, picture.type(large), email"]).start(completion: { connection, result, error  in
-                print("error", error)
-                print("connection", connection)
-                print("error", error)
-                  
-              })
-          }
-      }
+                         parameters: ["fields": "id, name, picture.type(large), email"]).start(completion: { connection, result, error  in
+                if error != nil {
+                    completion(nil)
+                }else {
+                    completion(result)
+                }
+            })
+        }
+    }
     
 }
 
 /*
  
  lass AppDelegate: NSObject, UIApplicationDelegate {
-     func application(_ application: UIApplication,
-                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-         ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
-     }
-     
-     func application(_ app: UIApplication,
-                      open url: URL,
-                      options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-         return ApplicationDelegate.shared.application(app, open: url, options: options)
-     }
+ func application(_ application: UIApplication,
+ didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+ ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
  }
-
+ 
+ func application(_ app: UIApplication,
+ open url: URL,
+ options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+ return ApplicationDelegate.shared.application(app, open: url, options: options)
+ }
+ }
+ 
  
  */
 
