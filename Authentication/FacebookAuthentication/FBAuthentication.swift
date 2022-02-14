7fddba9d71dc41afd39979f418f811b0d3e0d5fc
SaurabhPatel <40667243+SaurabPatel@users.noreply.github.com>
2022-02-15 01:16:05 +0530

    Update FBAuthentication.swift
    Create Struct to handle response

--- Authentication/FacebookAuthentication/FBAuthentication.swift
+++ Authentication/FacebookAuthentication/FBAuthentication.swift
@@ -2,10 +2,22 @@
 import Foundation
 import FBSDKLoginKit
 
+struct FBLoginUser {
+    var id: String?
+    var first_name: String?
+    var last_name: String?
+    var middle_name: String?
+    var name: String?
+    var name_format: String?
+    var picture: String?
+    var short_name: String?
+    var email: String?
+}
+
 class FBSignInHelper {
     
     public static var shared = FBSignInHelper()
-   
+    
     
     func signIn(user: @escaping (_ result: Any?) -> Void) {
         
@@ -17,6 +29,7 @@ class FBSignInHelper {
             getFBUserData(completion: { result in
                 user(result)
             })
+            return
         }
         //Get Currunt RootView
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
@@ -44,29 +57,46 @@ class FBSignInHelper {
                 if error != nil {
                     completion(nil)
                 }else {
-                    completion(result)
+                    completion(self.getFBLoginUser(result))
                 }
             })
         }
     }
     
+    private func getFBLoginUser(_ user: Any) -> FBLoginUser? {
+        if let json = user as? Dictionary<String, Any> {
+            if let email = json["email"] as? String {
+                print("\(email)")
+            }
+            if let firstName = json["first_name"] as? String {
+                print("\(firstName)")
+            }
+            if let lastName = json["last_name"] as? String {
+                print("\(lastName)")
+            }
+            if let id = json["id"] as? String {
+                print("\(id)")
+            }
+            
+            let id = json["id"] as? String
+            let lastName = json["last_name"] as? String
+            let firstName = json["first_name"] as? String
+            let middleName = json["middle_name"] as? String
+            let name = json["name"] as? String
+            let name_format = json["name_format"] as? String
+            let short_name = json["short_name"] as? String
+            let email = json["email"] as? String
+            var pictureURL: String? = nil
+            if let picture = json["picture"] as? [String: Any], let data = picture["data"] as? [String: Any], let url = data["url"] as? String {
+                pictureURL = url
+            }
+            let fbdUser = FBLoginUser(id: id, first_name: firstName, last_name: lastName, middle_name: middleName, name: name, name_format: name_format, picture: pictureURL, short_name: short_name, email: email)
+            return fbdUser
+        }else {
+            return nil
+        }
+        
+        
+    }
+    
 }
-
-/*
- 
- lass AppDelegate: NSObject, UIApplicationDelegate {
- func application(_ application: UIApplication,
- didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
- ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
- }
- 
- func application(_ app: UIApplication,
- open url: URL,
- options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
- return ApplicationDelegate.shared.application(app, open: url, options: options)
- }
- }
- 
- 
- */
-
